3c0f163821ec37e289f8963ade6288f6e02b1a39
Yogek 2022 <Yogek 2022@YOGEK>
2023-04-09 13:59:19 +0530

    Add project files.

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,28 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcServer;
+
+//var input = new HelloRequest { Name = "Tim" };
+//var channel = GrpcChannel.ForAddress("https://localhost:7003/");
+//var client = new Greeter.GreeterClient(channel);
+
+//var output = await client.SayHelloAsync(input);
+//Console.WriteLine(output.Message);
+//Console.ReadLine();
+
+var channel = GrpcChannel.ForAddress("https://localhost:7003/");
+var client = new Customer.CustomerClient(channel);
+
+//var output = await client.GetCustoemrInfoAsync(new CustomerLookupModel() { UserId = 1 });
+//Console.WriteLine(output);
+
+using (var output = client.GetCustoemrInfoStream(new CustomerTestModel()))
+{
+    while (await output.ResponseStream.MoveNext())
+    {
+        Console.WriteLine(output.ResponseStream.Current);
+    }
+
+}
+
+Console.ReadLine();
---
+++ GrpcServer/Services/CustomersService.cs
@@ -0,0 +1,67 @@
+﻿using Grpc.Core;
+
+namespace GrpcServer.Services
+{
+    public class CustomersService:Customer.CustomerBase
+    {
+        public readonly ILogger<CustomersService> _logger;
+        public CustomersService(ILogger<CustomersService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<customerModel> GetCustoemrInfo(CustomerLookupModel request, ServerCallContext context)
+        {
+            customerModel customerModel = new customerModel();
+
+            if(request.UserId==1)
+            {
+                customerModel.FirstName = "Yogek";
+                customerModel.LastName = "Garg";
+                customerModel.EmailAddress = "yogekgarg17@gmail.com";
+                customerModel.Age = 23;
+
+            }
+            else if(request.UserId==2)
+            {
+                customerModel.FirstName = "Abhinav";
+                customerModel.LastName = "Chnadla";
+                customerModel.EmailAddress = "abhichandla@gmail.com";
+                customerModel.Age = 23;
+                customerModel.IsAlive = "true";
+
+            }
+            return Task.FromResult(customerModel);
+        }
+
+        public override async Task GetCustoemrInfoStream(CustomerTestModel request, IServerStreamWriter<customerModel> responseStream, ServerCallContext context)
+        {
+            List<customerModel> customerlist = new List<customerModel>()
+            {
+                new customerModel()
+                {
+                      FirstName = "Yogek",
+                      LastName = "Garg",
+                      EmailAddress = "yogekgarg17@gmail.com",
+                      Age = 23
+                },
+                new customerModel()
+                {
+                      FirstName = "Abhinav",
+                      LastName = "Chandla",
+                      EmailAddress = "abhichandla@gmail.com",
+                      Age = 23
+                }
+            };
+
+            foreach(customerModel customer in customerlist)
+            {
+                await Task.Delay(1000);
+                await responseStream.WriteAsync(customer);
+            }
+            
+        }
+
+
+    }
+}
